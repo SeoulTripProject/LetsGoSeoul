ecd4848f40ffdd9609deba5efda23938acc9a14f
WangJi <WangJi@DESKTOP-U8QE4D3>
2021-09-11 11:24:53 +0900

    1111

--- LetsGoSeoul/src/com/sist/dao/MemberDAO.java
+++ LetsGoSeoul/src/com/sist/dao/MemberDAO.java
@@ -1,5 +1,60 @@
 package com.sist.dao;
-
+import java.util.*;
+import java.sql.*;
+import javax.sql.*;
+import javax.naming.*;
+import com.sist.dao.*;
 public class MemberDAO {
+	private Connection conn;
+	private PreparedStatement ps;
+	private static MemberDAO dao; 
+
+	public void getConnection()
+	{
+		try
+		{
+			Context init=new InitialContext();
+			Context c=(Context)init.lookup("java://comp//env");
+			DataSource ds=(DataSource)c.lookup("jdbc/oracle");
+			conn=ds.getConnection();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+
+	public void disConnection()
+	{
+		try
+		{
+			if(ps==null) ps.close();
+			if(conn==null) conn.close();
+		}catch(Exception ex) {}
+	}
+	
+	//싱글턴
+	public static MemberDAO newInstance()
+	{
+		if(dao==null) // 미생성시에는
+			dao=new MemberDAO();
+		return dao; // 이미 만들어진 dao객체를 사용한다
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
